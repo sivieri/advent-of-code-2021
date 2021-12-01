6f43866363a3c60f2fc36769176f7daa4fa221be
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2021-12-01 09:21:27 +0100

    Day 1 completed

---
+++ src/main/kotlin/me/sivieri/aoc2021/day1/Main1.kt
@@ -0,0 +1,18 @@
+package me.sivieri.aoc2021.day1
+
+import me.sivieri.aoc2021.Utils
+
+object Main1 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val data = Utils
+            .readInput(1)
+            .map { it.toInt() }
+        val result = data
+            .zipWithNext()
+            .count { it.second > it.first }
+        println(result)
+    }
+
+}
---
+++ src/main/kotlin/me/sivieri/aoc2021/day1/Main2.kt
@@ -0,0 +1,20 @@
+package me.sivieri.aoc2021.day1
+
+import me.sivieri.aoc2021.Utils
+
+object Main2 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val data = Utils
+            .readInput(1)
+            .map { it.toInt() }
+        val result = data
+            .windowed(size = 3, step = 1)
+            .map { it.sum() }
+            .zipWithNext()
+            .count { it.second > it.first }
+        println(result)
+    }
+
+}
