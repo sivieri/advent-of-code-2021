a4ca7331b236dfd0023d075eaf30aa92908a12c0
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2021-12-30 18:47:22 +0100

    Day 21 part 2 ideas

--- src/main/kotlin/me/sivieri/aoc2021/day21/QuantumGame.kt
+++ src/main/kotlin/me/sivieri/aoc2021/day21/QuantumGame.kt
@@ -6,32 +6,41 @@ class QuantumGame(
     player1: DicePlayer,
     player2: DicePlayer,
 ) {
-    private var games = listOf(
-        QuantumGameStatus(player1, player2, 0)
+    private var games = mutableMapOf(
+        QuantumGameStatus(player1, player2, 0) to 1L
     )
     private var player1games = 0L
     private var player2games = 0L
 
     fun play() {
         while (games.isNotEmpty()) {
-            games = games
-                .flatMap { game ->
+            println("Cache: ${games.size}")
+            println("Player 1 wins: $player1games")
+            println("Player 2 wins: $player2games")
+            val currentGames = games
+                .flatMap { (game, repetitions) ->
                     diceValues
                         .entries
                         .flatMap { (n, times) ->
-                            (0 until times).map { _ ->
+                            (0 until (times * repetitions)).map { _ ->
                                 game.play(n)
                             }
                         }
                 }
-                .mapNotNull { game ->
+            currentGames.forEach { game ->
+                if (games.containsKey(game)) games[game] = games[game]!! + 1L
+                else games[game] = 1L
+            }
+            val wonGames = games
+                .filter { (game, _) ->
                     if (game.isWon()) {
                         if (game.player1.score >= QuantumGameStatus.ENDGAME_SCORE) player1games++
                         else player2games++
-                        null
                     }
-                    else game
+                    game.isWon()
                 }
+                .keys
+            wonGames.forEach { games.remove(it) }
         }
     }
 
